03c37ecda29055e894bd0cfa344f0e8bb95a1aba
Akshanshu Pal <akshanshupal@Akshanshus-MacBook-Air.local>
2024-08-06 10:47:27 +0530

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import type { AppProps } from "next/app";
+
+import { NextUIProvider } from "@nextui-org/system";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRouter } from "next/router";
+import DefaultLayout from "@/layouts/default";
+
+
+import { fontSans, fontMono } from "@/config/fonts";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  return (
+    <NextUIProvider navigate={router.push}>
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
+      {/* <NextThemesProvider>
+      </NextThemesProvider> */}
+    </NextUIProvider>
+  );
+}
+
+export const fonts = {
+  sans: fontSans.style.fontFamily,
+  mono: fontMono.style.fontFamily,
+};
